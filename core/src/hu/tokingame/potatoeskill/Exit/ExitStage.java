b51a31e09da62fdf0bae1b3ea6b37fbc5ab23e14
mrclsu <marcell.schuh@gmail.com>
2017-12-04 18:38:18 +0100

    Zoli hülye
    Nem használunk System.exit(0) soha ha mégyegyszer meglátom megleszel verve

--- core/src/hu/tokingame/potatoeskill/Exit/ExitStage.java
+++ core/src/hu/tokingame/potatoeskill/Exit/ExitStage.java
@@ -1,5 +1,6 @@
 package hu.tokingame.potatoeskill.Exit;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -40,7 +41,7 @@ public class ExitStage extends MyStage {
                     @Override
                     public void clicked(InputEvent event, float x, float y) {
                         super.clicked(event, x, y);
-                        System.exit(0);
+                        Gdx.app.exit();
                     }
                 });
             }
